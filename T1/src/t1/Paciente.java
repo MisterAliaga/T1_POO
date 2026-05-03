256c07e100d0f6f8e8c1c01a928ae93dc33bac3e
Mr.Aliaga <n00460852@upn.pe>
2026-05-02 20:34:43 -0500

    Implementación de clase Paciente

---
+++ T1/src/t1/Paciente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1;
+
+/**
+ *
+ * @author UCB10202
+ */
+public class Paciente {
+    
+}
