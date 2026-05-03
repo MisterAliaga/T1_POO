745eeab0faadf6a10b9a9154553f6d29fe4f845a
Mr.Aliaga <n00460852@upn.pe>
2026-05-02 20:49:31 -0500

    Ingreso de Get y Set

--- T1/src/t1/Paciente.java
+++ T1/src/t1/Paciente.java
@@ -9,5 +9,80 @@ package t1;
  * @author UCB10202
  */
 public class Paciente {
-    
+    private String nombre_completo;
+    private String tipo_doc;
+    private String nmr_doc;
+    private String fecha_nac;
+    private String tipo_sangre;
+    private String alergias;
+    private String telefono;
+    private String correo;
+
+    public String getNombre_completo() {
+        return nombre_completo;
+    }
+
+    public void setNombre_completo(String nombre_completo) {
+        this.nombre_completo = nombre_completo;
+    }
+
+    public String getTipo_doc() {
+        return tipo_doc;
+    }
+
+    public void setTipo_doc(String tipo_doc) {
+        this.tipo_doc = tipo_doc;
+    }
+
+    public String getNmr_doc() {
+        return nmr_doc;
+    }
+
+    public void setNmr_doc(String nmr_doc) {
+        this.nmr_doc = nmr_doc;
+    }
+
+    public String getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(String fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public String getTipo_sangre() {
+        return tipo_sangre;
+    }
+
+    public void setTipo_sangre(String tipo_sangre) {
+        this.tipo_sangre = tipo_sangre;
+    }
+
+    public String getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Paciente() {
+        
+    }
 }
